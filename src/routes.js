f410c1f58504ea99fc8a55d281d13f9231241200
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-01-14 20:42:25 -0800

    added mongodb  connection

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 
 const routes = Router();
 
-// Routes
+// Routes: Get; Post; Put; Delete
 
 routes.get('/', (req,res) => {
-    return res.send('oi');
+    return res.send('oi ju');
 })
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 class App {
     constructor() {
         this.server = express();
 
+        this.database();
         this.middlewares();
         this.routes();
+    }
+
+    database() {
+        mongoose.connect("mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}>@schlickmannapps-unyax.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority", {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
     }
 
     middlewares() {
